b99208eff7afca8dd56be163061a8adff48348cf
Andrii Murashkin <amu@adorsys.com.ua>
2019-06-13 15:41:10 +0300

    #865 applied global consent for account related services

--- xs2a-impl/src/test/java/de/adorsys/psd2/xs2a/web/link/AccountDetailsLinksTest.java
+++ xs2a-impl/src/test/java/de/adorsys/psd2/xs2a/web/link/AccountDetailsLinksTest.java
@@ -31,12 +31,14 @@ public class AccountDetailsLinksTest {
     private static final String WRONG_ACCOUNT_ID = "wrong_account_id";
 
     private Xs2aAccountAccess xs2aAccountAccess;
+    private Xs2aAccountAccess xs2aAccountAccessGlobal;
     private Links expectedLinks;
 
     @Before
     public void setUp() {
         JsonReader jsonReader = new JsonReader();
         xs2aAccountAccess = jsonReader.getObjectFromFile("json/link/account_access.json", Xs2aAccountAccess.class);
+        xs2aAccountAccessGlobal = jsonReader.getObjectFromFile("json/link/account_access-global.json", Xs2aAccountAccess.class);
         expectedLinks = new Links();
     }
 
@@ -46,6 +48,15 @@ public class AccountDetailsLinksTest {
 
         expectedLinks.setBalances(new HrefType("http://url/v1/accounts/33333-999999999/balances"));
         expectedLinks.setTransactions(new HrefType("http://url/v1/accounts/33333-999999999/transactions"));
+        assertEquals(expectedLinks, links);
+    }
+
+    @Test
+    public void create_globalConsent_success() {
+        AccountDetailsLinks links = new AccountDetailsLinks(HTTP_URL, ACCOUNT_ID, xs2aAccountAccessGlobal);
+
+        expectedLinks.setBalances("http://url/v1/accounts/33333-999999999/balances");
+        expectedLinks.setTransactions("http://url/v1/accounts/33333-999999999/transactions");
         assertEquals(expectedLinks, links);
     }
 
