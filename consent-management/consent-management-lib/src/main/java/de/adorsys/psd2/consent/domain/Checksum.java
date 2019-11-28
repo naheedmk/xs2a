8229eb7fad800483c0da301ba26a884a24e2f062
ske <ske@adorsys.de>
2019-11-28 11:39:05 +0100

    Changed array to Set

--- consent-management/consent-management-lib/src/main/java/de/adorsys/psd2/consent/domain/Checksum.java
+++ consent-management/consent-management-lib/src/main/java/de/adorsys/psd2/consent/domain/Checksum.java
@@ -4,31 +4,29 @@ import de.adorsys.psd2.consent.service.ChecksumType;
 import de.adorsys.psd2.consent.service.ChecksumValue;
 
 import javax.persistence.*;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @Embeddable
 
 public class Checksum {
     @ElementCollection
-    @OrderColumn
-    @CollectionTable(name="ais_consent_checksum")
+    @CollectionTable(name = "ais_consent_checksum")
     @JoinColumn(name = "checksum_id", referencedColumnName = "id")
-    private ChecksumValue[] values = {
-        new ChecksumValue(ChecksumType.ACCOUNT_ACCESSES),
-        new ChecksumValue(ChecksumType.ASPSP_ACCOUNT_ACCESSES),
-        new ChecksumValue(ChecksumType.RECURRING_INDICATOR),
-        new ChecksumValue(ChecksumType.VALID_UNTIL),
-        new ChecksumValue(ChecksumType.FREQUENCY_PER_DAY),
-        new ChecksumValue(ChecksumType.COMBINED_SERVICE_INDICATOR)
-    };
+    private Set<ChecksumValue> values;
 
     @Transient
     public Map<ChecksumType, ChecksumValue> container;
 
     public Checksum() {
-        this.container = new HashMap<>();
+        this.values = new HashSet<>();
+        Collections.addAll(values, new ChecksumValue(ChecksumType.ACCOUNT_ACCESSES),
+                           new ChecksumValue(ChecksumType.ASPSP_ACCOUNT_ACCESSES),
+                           new ChecksumValue(ChecksumType.RECURRING_INDICATOR),
+                           new ChecksumValue(ChecksumType.VALID_UNTIL),
+                           new ChecksumValue(ChecksumType.FREQUENCY_PER_DAY),
+                           new ChecksumValue(ChecksumType.COMBINED_SERVICE_INDICATOR));
 
+        this.container = new HashMap<>();
         for (ChecksumValue value : values) {
             container.put(value.getChecksumType(), value);
         }
