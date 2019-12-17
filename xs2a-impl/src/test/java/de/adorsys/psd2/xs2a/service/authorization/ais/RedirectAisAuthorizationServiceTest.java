cecfe474ec7d54f5ee6b877f1d60e0ef8f335473
Hryhorii Hevorkian <hhe@adorsys.com.ua>
2019-12-17 11:19:08 +0200

    #1097 added test.

--- xs2a-impl/src/test/java/de/adorsys/psd2/xs2a/service/authorization/ais/RedirectAisAuthorizationServiceTest.java
+++ xs2a-impl/src/test/java/de/adorsys/psd2/xs2a/service/authorization/ais/RedirectAisAuthorizationServiceTest.java
@@ -20,9 +20,16 @@ import de.adorsys.psd2.consent.api.ais.CreateAisConsentAuthorizationResponse;
 import de.adorsys.psd2.xs2a.core.profile.ScaApproach;
 import de.adorsys.psd2.xs2a.core.psu.PsuIdData;
 import de.adorsys.psd2.xs2a.core.sca.ScaStatus;
+import de.adorsys.psd2.xs2a.domain.ResponseObject;
+import de.adorsys.psd2.xs2a.domain.authorisation.UpdateAuthorisationRequest;
 import de.adorsys.psd2.xs2a.domain.consent.AccountConsentAuthorization;
 import de.adorsys.psd2.xs2a.domain.consent.CreateConsentAuthorizationResponse;
+import de.adorsys.psd2.xs2a.domain.consent.UpdateConsentPsuDataReq;
+import de.adorsys.psd2.xs2a.domain.consent.UpdateConsentPsuDataResponse;
+import de.adorsys.psd2.xs2a.service.authorization.processor.model.AuthorisationProcessorResponse;
 import de.adorsys.psd2.xs2a.service.consent.Xs2aAisConsentService;
+import de.adorsys.psd2.xs2a.service.mapper.consent.Xs2aAisConsentMapper;
+import de.adorsys.xs2a.reader.JsonReader;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -32,6 +39,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -51,6 +59,14 @@ public class RedirectAisAuthorizationServiceTest {
     @Mock
     private Xs2aAisConsentService xs2aAisConsentService;
 
+    @Mock
+    private Xs2aAisConsentMapper xs2aAisConsentMapper;
+
+    @Mock
+    private AisAuthorisationConfirmationService aisAuthorisationConfirmationService;
+
+    private JsonReader jsonReader = new JsonReader();
+
     @Test
     public void createConsentAuthorization_success() {
         // Given
@@ -63,6 +79,28 @@ public class RedirectAisAuthorizationServiceTest {
         // Then
         assertThat(actualResponse.isPresent()).isTrue();
         assertThat(actualResponse).isEqualTo(Optional.of(CREATE_CONSENT_AUTHORIZATION_RESPONSE));
+    }
+
+    @Test
+    public void updateConsentPsuData_success() {
+        // Given
+        UpdateConsentPsuDataReq updateConsentPsuDataReq = buildUpdateConsentPsuDataRequest();
+
+        when(xs2aAisConsentMapper.mapToSpiUpdateConsentPsuDataReq(any(UpdateAuthorisationRequest.class), any(AuthorisationProcessorResponse.class)))
+            .thenReturn(updateConsentPsuDataReq);
+
+        when(aisAuthorisationConfirmationService.processAuthorisationConfirmation(updateConsentPsuDataReq)).thenReturn(getUpdateConsentResponseObject());
+
+        UpdateAuthorisationRequest request = new UpdateConsentPsuDataReq();
+        AuthorisationProcessorResponse response = new AuthorisationProcessorResponse();
+
+        // When
+        AuthorisationProcessorResponse actualResponse = redirectAisAuthorisationService.updateConsentPsuData(request, response);
+
+        // Then
+        assertThat(actualResponse).isNotNull();
+        assertThat(actualResponse.getScaStatus()).isEqualTo(ScaStatus.FINALISED);
+        assertThat(actualResponse.getConsentId()).isEqualTo(CONSENT_ID);
     }
 
     @Test
@@ -136,5 +174,15 @@ public class RedirectAisAuthorizationServiceTest {
 
     private CreateAisConsentAuthorizationResponse buildCreateAisConsentAuthorizationResponse() {
         return new CreateAisConsentAuthorizationResponse(AUTHORISATION_ID, ScaStatus.RECEIVED, INTERNAL_REQUEST_ID, PSU_ID_DATA);
+    }
+
+    private ResponseObject<UpdateConsentPsuDataResponse> getUpdateConsentResponseObject() {
+        return ResponseObject.<UpdateConsentPsuDataResponse>builder()
+                   .body((new UpdateConsentPsuDataResponse(ScaStatus.FINALISED, CONSENT_ID, AUTHORISATION_ID))).build();
+    }
+
+    private UpdateConsentPsuDataReq buildUpdateConsentPsuDataRequest () {
+        return jsonReader.getObjectFromFile("json/service/mapper/update-consent-psu-data-req-with-password.json",
+                                                                        UpdateConsentPsuDataReq.class);
     }
 }
