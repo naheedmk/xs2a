fc3df396d2b5409741f52fce5c0ef89b9d7a35a7
ske <ske@adorsys.de>
2020-01-09 14:08:01 +0100

    Add possibility of disabling the schedule + update release notes

--- aspsp-profile/aspsp-profile-lib/src/main/java/de/adorsys/psd2/aspsp/profile/service/BankProfileReloadingScheduleTask.java
+++ aspsp-profile/aspsp-profile-lib/src/main/java/de/adorsys/psd2/aspsp/profile/service/BankProfileReloadingScheduleTask.java
@@ -1,9 +1,9 @@
 package de.adorsys.psd2.aspsp.profile.service;
 
-import de.adorsys.psd2.aspsp.profile.config.ProfileConfiguration;
-import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
+    import de.adorsys.psd2.aspsp.profile.config.ProfileConfiguration;
+    import lombok.RequiredArgsConstructor;
+    import org.springframework.scheduling.annotation.Scheduled;
+    import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
@@ -11,7 +11,7 @@ public class BankProfileReloadingScheduleTask {
     private final BankProfileReadingService bankProfileReadingService;
     private final ProfileConfiguration profileConfiguration;
 
-    @Scheduled(fixedDelayString = "${reload-interval:60000}")
+    @Scheduled(cron = "${aspsp-profile-reload.cron.expression:-}")
     public void updateProfileConfiguration() {
         ProfileConfiguration newProfileConfiguration = bankProfileReadingService.getProfileConfiguration();
         profileConfiguration.setSetting(newProfileConfiguration.getSetting());
