1ed70cb33f70f085132efda82ef2291037e599d1
rain2581 <kan@adorsys.com.ua>
2020-01-24 12:49:09 +0200

    add curl check status

--- scripts/notifications/webhook_slack.sh
+++ scripts/notifications/webhook_slack.sh
@@ -7,8 +7,7 @@ set -eu
 ##ASPSP_PROFILE_LINK="https://dev-psd2-aspspprofile.cloud.adorsys.de/actuator/info"
 ##HELM_RELEASE="dev-psd2"
 ##ENV_JSON_FILE="psd2-env.json"
-##PRINT VARS
-#echo $LEDGERS_LINK\n$CMS_LINK\n$CONNECTOR_LINK\n$ASPSP_PROFILE_LINK\n$ENV_JSON_FILE
+##SLACK_CHANNEL="htpps://"
 
 # Define array of arguments
 myArray=( "$@" )
@@ -31,31 +30,26 @@ do
   VERSION=$(echo $JSON | jq -r .version)
   #get the build number
   BUILD=$(echo $JSON | jq -r .build.number)
-  #echo $BUILD
   # remove Build: from build.number
   BUILD=${BUILD//Build:/}
-  #Echo $BUILD
   #create json element one per argument 
   for i in "${!argsArray[@]}"
   do
     if [[ $i == $item_from_array ]] && [ "$BUILD" != ""null ]; then 
       PIPELINE_LINK=${argsArray[$i]}
-      #echo "$i and $item_from_array and $BUILD" 
       ONE_STRING_FIELD="{\"type\": \"mrkdwn\",\"text\": \"*$item_from_array:*\n Version: $VERSION \n build: $BUILD\n <$PIPELINE_LINK/pipelines/${BUILD}|Pipeline build link>\"}"
     fi
     if [[ $i == $item_from_array ]] && [[ "$BUILD" == "null" ]]; then
-      #echo "ELSE $i and $item_from_array and $BUILD" 
       ONE_STRING_FIELD="{\"type\": \"mrkdwn\",\"text\": \"*$item_from_array:*\n Version: $VERSION \n build: $BUILD\n No Pipeline build link\"}"
     fi
   done
-  #ONE_STRING_FIELD="{\"type\": \"mrkdwn\",\"text\": \"*$item_from_array:*\n Version: $VERSION \n build: $BUILD\n <$LINK2|Pipeline build link>\"}"
   #add to default json template into block.fields array one json element and use tmp file
   BASE_JSON=$(jq --argjson blockElement "$ONE_STRING_FIELD" '.blocks[2].fields += [$blockElement]' tmp.json)
 done
-#echo $BASE_JSON
-
-#post to slack
+#post to slack and check of not ok message then exit with error
 STATUS_CODE=$(curl -sS -X POST -H 'Content-type: application/json' --data "$BASE_JSON" "$SLACK_CHANNEL")
 echo $STATUS_CODE
-
+if [ "$STATUS_CODE" != "ok" ] ; then exit 1
+else
 exit 0
+fi
