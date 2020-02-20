35979c57d2d95a1533a6e911c2ef966b52fb69a7
ske <ske@adorsys.de>
2020-02-20 09:41:59 +0100

    Removed @ApiModelProperty annotations from CreateConsentReq

--- xs2a-impl/src/main/java/de/adorsys/psd2/xs2a/domain/consent/CreateConsentReq.java
+++ xs2a-impl/src/main/java/de/adorsys/psd2/xs2a/domain/consent/CreateConsentReq.java
@@ -37,33 +37,27 @@ import static de.adorsys.psd2.xs2a.core.ais.AccountAccessType.ALL_ACCOUNTS;
 @Data
 @ApiModel(description = "Request creates an account information consent resource at the ASPSP regarding access to accounts specified in this request")
 public class CreateConsentReq implements AccountReferenceCollector {
-
-    @ApiModelProperty(value = "Requested access services.", required = true)
     @NotNull
     private AccountAccess access;
 
-    // ToDo add proper annotations https://git.adorsys.de/adorsys/xs2a/aspsp-xs2a/issues/1170
     private AccountAccessType availableAccounts;
+
     private AccountAccessType allPsd2;
+
     private AccountAccessType availableAccountsWithBalance;
 
-    @ApiModelProperty(value = "'true', if the consent is for recurring access to the account data , 'false', if the consent is for one access to the account data", required = true)
     @NotNull
     private boolean recurringIndicator;
 
-    @ApiModelProperty(value = "This parameter is requesting a valid until date for the requested consent. The content is the local ASPSP date in ISODate Format", required = true, example = "2019-10-30")
     @NotNull
     private LocalDate validUntil;
 
-    @ApiModelProperty(value = "This field indicates the requested maximum frequency for an access per day. For a once-off access, this attribute is set to 1", required = true, example = "4")
     @NotNull
     private int frequencyPerDay;
 
-    @ApiModelProperty(value = "If 'true' indicates that a payment initiation service will be addressed in the same 'session'", required = true)
     @NotNull
     private boolean combinedServiceIndicator;
 
-    @ApiModelProperty(value = "TPP redirect URI object'")
     private TppRedirectUri tppRedirectUri;
 
     private TppNotificationData tppNotificationData;
