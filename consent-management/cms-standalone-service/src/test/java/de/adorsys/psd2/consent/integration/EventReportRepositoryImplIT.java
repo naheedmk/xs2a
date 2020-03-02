d1acaffac65fe621ed5bd9c61f66ca072570037f
ske <ske@adorsys.de>
2020-03-02 11:53:53 +0100

    Fixed disabled tests in EventReportRepositoryImplIT

--- consent-management/cms-standalone-service/src/test/java/de/adorsys/psd2/consent/integration/EventReportRepositoryImplIT.java
+++ consent-management/cms-standalone-service/src/test/java/de/adorsys/psd2/consent/integration/EventReportRepositoryImplIT.java
@@ -14,12 +14,17 @@
  * limitations under the License.
  */
 
-package de.adorsys.psd2.report;
+package de.adorsys.psd2.consent.integration;
 
+import com.google.common.base.Charsets;
+import de.adorsys.psd2.consent.integration.config.IntegrationTestConfiguration;
 import de.adorsys.psd2.event.core.model.EventOrigin;
 import de.adorsys.psd2.event.core.model.EventType;
+import de.adorsys.psd2.event.persist.EventReportRepository;
+import de.adorsys.psd2.event.persist.EventRepository;
 import de.adorsys.psd2.event.persist.model.EventPO;
 import de.adorsys.psd2.event.persist.model.ReportEvent;
+import de.adorsys.psd2.report.EventReportRepositoryImpl;
 import de.adorsys.xs2a.reader.JsonReader;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
@@ -27,27 +32,37 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import javax.persistence.EntityManager;
 import java.time.OffsetDateTime;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@ActiveProfiles("integration-test")
 @ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = IntegrationTestConfiguration.class)
 @DataJpaTest
-@ContextConfiguration(classes = TestDBConfiguration.class)
 class EventReportRepositoryImplIT {
     private static final String INSTANCE_ID = "3de76f19-1df7-44d8-b760-ca972d2f945c";
     private static final String CONSENT_ID = "fa6e687b-1ac9-4b1a-9c74-357c35c82ba1";
     private static final String PAYMENT_ID = "j-t4XyLJTzQkonfSTnyxIMc";
-    private static final byte[] PAYLOAD = "payload".getBytes();
+    private static final byte[] PAYLOAD = "payload".getBytes(Charsets.UTF_8);
     private static final OffsetDateTime CREATED_DATETIME = OffsetDateTime.now();
 
     @Autowired
-    private EventReportRepositoryImpl repository;
+    private EventReportRepository repository;
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    @Autowired
+    private EntityManager entityManager;
+
     private JsonReader jsonReader = new JsonReader();
     private EventPO eventPO;
     private OffsetDateTime start;
@@ -61,50 +76,66 @@ class EventReportRepositoryImplIT {
 
         start = CREATED_DATETIME.minusHours(1);
         end = CREATED_DATETIME.plusHours(1);
+
+        populateWithInitialData();
+    }
+
+    private void populateWithInitialData() {
+        eventRepository.save(eventPO);
+        entityManager.flush();
     }
 
     @Test
-    @Disabled
     void getEventsForPeriod() {
         List<ReportEvent> eventsForPeriod = repository.getEventsForPeriod(start, end, INSTANCE_ID);
         assertNotNull(eventsForPeriod);
         assertEquals(1, eventsForPeriod.size());
-        assertEquals(eventPO, eventsForPeriod.get(0));
+        assertEvents(eventPO, eventsForPeriod.get(0));
     }
 
     @Test
-    @Disabled
     void getEventsForPeriodAndConsentId() {
         List<ReportEvent> eventsForPeriod = repository.getEventsForPeriodAndConsentId(start, end, CONSENT_ID, INSTANCE_ID);
         assertNotNull(eventsForPeriod);
         assertEquals(1, eventsForPeriod.size());
-        assertEquals(eventPO, eventsForPeriod.get(0));
+        assertEvents(eventPO, eventsForPeriod.get(0));
     }
 
     @Test
-    @Disabled
     void getEventsForPeriodAndPaymentId() {
         List<ReportEvent> eventsForPeriod = repository.getEventsForPeriodAndPaymentId(start, end, PAYMENT_ID, INSTANCE_ID);
         assertNotNull(eventsForPeriod);
         assertEquals(1, eventsForPeriod.size());
-        assertEquals(eventPO, eventsForPeriod.get(0));
+        assertEvents(eventPO, eventsForPeriod.get(0));
     }
 
     @Test
-    @Disabled
     void getEventsForPeriodAndEventOrigin() {
         List<ReportEvent> eventsForPeriod = repository.getEventsForPeriodAndEventOrigin(start, end, EventOrigin.TPP, INSTANCE_ID);
         assertNotNull(eventsForPeriod);
         assertEquals(1, eventsForPeriod.size());
-        assertEquals(eventPO, eventsForPeriod.get(0));
+        assertEvents(eventPO, eventsForPeriod.get(0));
     }
 
     @Test
-    @Disabled
     void getEventsForPeriodAndEventType() {
         List<ReportEvent> eventsForPeriod = repository.getEventsForPeriodAndEventType(start, end, EventType.PAYMENT_INITIATION_REQUEST_RECEIVED, INSTANCE_ID);
         assertNotNull(eventsForPeriod);
         assertEquals(1, eventsForPeriod.size());
-        assertEquals(eventPO, eventsForPeriod.get(0));
+        assertEvents(eventPO, eventsForPeriod.get(0));
+    }
+
+    private void assertEvents(EventPO eventPO, ReportEvent reportEvent) {
+        assertEquals(eventPO.getTimestamp(), reportEvent.getTimestamp());
+        assertEquals(eventPO.getConsentId(), reportEvent.getConsentId());
+        assertEquals(eventPO.getPaymentId(), reportEvent.getPaymentId());
+        assertEquals(new String(eventPO.getPayload()), new String(reportEvent.getPayload()));
+        assertEquals(eventPO.getEventOrigin(), reportEvent.getEventOrigin());
+        assertEquals(eventPO.getEventType(), reportEvent.getEventType());
+        assertEquals(eventPO.getInstanceId(), reportEvent.getInstanceId());
+        assertEquals(eventPO.getTppAuthorisationNumber(), reportEvent.getTppAuthorisationNumber());
+        assertEquals(eventPO.getXRequestId(), reportEvent.getXRequestId());
+        assertEquals(eventPO.getPsuIdData(), reportEvent.getPsuIdData().iterator().next());
+        assertEquals(eventPO.getInternalRequestId(), reportEvent.getInternalRequestId());
     }
 }
