56947a3490891c47d9293db1e6e0ee036a307380
kyaex <kya@adorsys.com.ua>
2020-04-01 15:41:15 +0300

    #1237 part2: changed AdditionalInformationSupportedService according to discussion

--- xs2a-impl/src/main/java/de/adorsys/psd2/xs2a/service/AdditionalInformationSupportedService.java
+++ xs2a-impl/src/main/java/de/adorsys/psd2/xs2a/service/AdditionalInformationSupportedService.java
@@ -16,18 +16,57 @@
 
 package de.adorsys.psd2.xs2a.service;
 
+import de.adorsys.psd2.core.data.AccountAccess;
+import de.adorsys.psd2.xs2a.core.profile.AdditionalInformationAccess;
 import de.adorsys.psd2.xs2a.domain.consent.CreateConsentReq;
+import de.adorsys.psd2.xs2a.service.profile.AspspProfileServiceWrapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import static de.adorsys.psd2.xs2a.core.ais.AccountAccessType.ALL_ACCOUNTS;
+import static de.adorsys.psd2.xs2a.core.ais.AccountAccessType.ALL_ACCOUNTS_WITH_OWNER_NAME;
 
 @Service
 @RequiredArgsConstructor
 public class AdditionalInformationSupportedService {
-    private final AccountOwnerInformationService accountOwnerInformationService;
-    private final TrustedBeneficiariesInformationService trustedBeneficiariesInformationService;
+    private final AspspProfileServiceWrapper aspspProfileService;
 
     public CreateConsentReq checkIfAdditionalInformationSupported(CreateConsentReq request) {
-        CreateConsentReq requestAfterOwnerNameCheck = accountOwnerInformationService.checkIfAdditionalInformationSupported(request);
-        return trustedBeneficiariesInformationService.checkIfAdditionalInformationSupported(requestAfterOwnerNameCheck);
+        boolean isOwnerNameSupported = aspspProfileService.isAccountOwnerInformationSupported();
+        boolean isTrustedBeneficiariesSupported = aspspProfileService.isTrustedBeneficiariesSupported();
+
+        if (!isOwnerNameSupported) {
+            clearAccountAccessType(request);
+        }
+
+        if (!isOwnerNameSupported || !isTrustedBeneficiariesSupported) {
+            AccountAccess access = request.getAccess();
+            AdditionalInformationAccess additionalInformationAccess = access.getAdditionalInformationAccess();
+            if (additionalInformationAccess != null) {
+                AdditionalInformationAccess additionalInformationCleaned = new AdditionalInformationAccess(
+                    isOwnerNameSupported ? additionalInformationAccess.getOwnerName() : null,
+                    isTrustedBeneficiariesSupported ? additionalInformationAccess.getTrustedBeneficiaries() : null);
+                request.setAccess(new AccountAccess(access.getAccounts(),
+                                                    access.getBalances(),
+                                                    access.getTransactions(),
+                                                    additionalInformationCleaned));
+            }
+        }
+
+        return request;
+    }
+
+    private CreateConsentReq clearAccountAccessType(CreateConsentReq request) {
+        if (request.getAvailableAccounts() == ALL_ACCOUNTS_WITH_OWNER_NAME) {
+            request.setAvailableAccounts(ALL_ACCOUNTS);
+        }
+        if (request.getAvailableAccountsWithBalance() == ALL_ACCOUNTS_WITH_OWNER_NAME) {
+            request.setAvailableAccountsWithBalance(ALL_ACCOUNTS);
+        }
+        if (request.getAllPsd2() == ALL_ACCOUNTS_WITH_OWNER_NAME) {
+            request.setAllPsd2(ALL_ACCOUNTS);
+        }
+
+        return request;
     }
 }
--- xs2a-impl/src/test/java/de/adorsys/psd2/xs2a/service/AdditionalInformationSupportedServiceTest.java
+++ xs2a-impl/src/test/java/de/adorsys/psd2/xs2a/service/AdditionalInformationSupportedServiceTest.java
@@ -17,6 +17,7 @@
 package de.adorsys.psd2.xs2a.service;
 
 import de.adorsys.psd2.xs2a.domain.consent.CreateConsentReq;
+import de.adorsys.psd2.xs2a.service.profile.AspspProfileServiceWrapper;
 import de.adorsys.xs2a.reader.JsonReader;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -30,27 +31,125 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 public class AdditionalInformationSupportedServiceTest {
     @InjectMocks
-    private AdditionalInformationSupportedService additionalInformationSupportedService;
+    private AdditionalInformationSupportedService service;
     @Mock
-    private AccountOwnerInformationService accountOwnerInformationService;
-    @Mock
-    private TrustedBeneficiariesInformationService trustedBeneficiariesInformationService;
+    private AspspProfileServiceWrapper aspspProfileService;
 
     JsonReader jsonReader = new JsonReader();
 
+
     @Test
-    void checkIfAdditionalInformationSupported() {
+    void checkSupportedAccountTrustedBeneficiariesInformation_supported() {
         // Given
-        CreateConsentReq input = jsonReader.getObjectFromFile("json/service/create-consent-req-with-beneficiaries-and-owner-name.json", CreateConsentReq.class);
-        CreateConsentReq createConsentReqAfterOwnerNameCut = jsonReader.getObjectFromFile("json/service/create-consent-req-with-beneficiaries.json", CreateConsentReq.class);
-        CreateConsentReq expected = jsonReader.getObjectFromFile("json/service/create-consent-req.json", CreateConsentReq.class);
-        when(accountOwnerInformationService.checkIfAdditionalInformationSupported(input)).thenReturn(createConsentReqAfterOwnerNameCut);
-        when(trustedBeneficiariesInformationService.checkIfAdditionalInformationSupported(createConsentReqAfterOwnerNameCut)).thenReturn(expected);
+        CreateConsentReq input = jsonReader.getObjectFromFile("json/service/create-consent-req-with-beneficiaries.json", CreateConsentReq.class);
+        when(aspspProfileService.isTrustedBeneficiariesSupported()).thenReturn(true);
 
         // When
-        CreateConsentReq actual = additionalInformationSupportedService.checkIfAdditionalInformationSupported(input);
+        CreateConsentReq actual = service.checkIfAdditionalInformationSupported(input);
+
+        // Then
+        assertThat(actual).isEqualTo(input);
+    }
+
+    @Test
+    void checkSupportedAccountTrustedBeneficiariesInformation_notSupportedAndPresent() {
+        // Given
+        CreateConsentReq input = jsonReader.getObjectFromFile("json/service/create-consent-req-with-beneficiaries.json", CreateConsentReq.class);
+        CreateConsentReq expected = jsonReader.getObjectFromFile("json/service/create-consent-req.json", CreateConsentReq.class);
+        when(aspspProfileService.isTrustedBeneficiariesSupported()).thenReturn(false);
+
+        // When
+        CreateConsentReq actual = service.checkIfAdditionalInformationSupported(input);
 
         // Then
         assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void checkSupportedAccountTrustedBeneficiariesInformation_notSupportedAndNotPresent() {
+        // Given
+        CreateConsentReq input = jsonReader.getObjectFromFile("json/service/create-consent-req.json", CreateConsentReq.class);
+        when(aspspProfileService.isTrustedBeneficiariesSupported()).thenReturn(false);
+
+        // When
+        CreateConsentReq actual = service.checkIfAdditionalInformationSupported(input);
+
+        // Then
+        assertThat(actual).isEqualTo(input);
+    }
+
+
+    @Test
+    void checkSupportedAccountOwnerInformation_ownerNameNotSupported_WithOwnerName() {
+        // Given
+        CreateConsentReq expected = jsonReader.getObjectFromFile("json/service/create-consent-req.json", CreateConsentReq.class);
+        when(aspspProfileService.isAccountOwnerInformationSupported()).thenReturn(false);
+
+        CreateConsentReq inputData = jsonReader.getObjectFromFile("json/service/create-consent-req-with-owner-name.json", CreateConsentReq.class);
+
+        // When
+        CreateConsentReq actual = service.checkIfAdditionalInformationSupported(inputData);
+
+        // Then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void checkSupportedAccountOwnerInformation_ownerNameNotSupported_WithAvailableAccounts() {
+        // Given
+        CreateConsentReq expected = jsonReader.getObjectFromFile("json/service/create-consent-req-with-available-accounts.json", CreateConsentReq.class);
+        when(aspspProfileService.isAccountOwnerInformationSupported()).thenReturn(false);
+
+        CreateConsentReq inputData = jsonReader.getObjectFromFile("json/service/create-consent-req-with-available-accounts-owner-name.json", CreateConsentReq.class);
+
+        // When
+        CreateConsentReq actual = service.checkIfAdditionalInformationSupported(inputData);
+
+        // Then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void checkSupportedAccountOwnerInformation_ownerNameNotSupported_WithAvailableAccountsWithBalances() {
+        // Given
+        CreateConsentReq expected = jsonReader.getObjectFromFile("json/service/create-consent-req-with-balance.json", CreateConsentReq.class);
+        when(aspspProfileService.isAccountOwnerInformationSupported()).thenReturn(false);
+
+        CreateConsentReq inputData = jsonReader.getObjectFromFile("json/service/create-consent-req-with-available-accounts-with-balance-owner-name.json", CreateConsentReq.class);
+
+        // When
+        CreateConsentReq actual = service.checkIfAdditionalInformationSupported(inputData);
+
+        // Then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void checkSupportedAccountOwnerInformation_ownerNameNotSupported_WithAllPsd2() {
+        // Given
+        CreateConsentReq expected = jsonReader.getObjectFromFile("json/service/create-consent-req-with-all-psd2.json", CreateConsentReq.class);
+        when(aspspProfileService.isAccountOwnerInformationSupported()).thenReturn(false);
+
+        CreateConsentReq inputData = jsonReader.getObjectFromFile("json/service/create-consent-req-with-all-psd2-owner-name.json", CreateConsentReq.class);
+
+        // When
+        CreateConsentReq actual = service.checkIfAdditionalInformationSupported(inputData);
+
+        // Then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void checkSupportedAccountOwnerInformation_ownerNameSupported() {
+        // Given
+        when(aspspProfileService.isAccountOwnerInformationSupported()).thenReturn(true);
+
+        CreateConsentReq inputData = jsonReader.getObjectFromFile("json/service/create-consent-req-with-owner-name.json", CreateConsentReq.class);
+
+        // When
+        CreateConsentReq actual = service.checkIfAdditionalInformationSupported(inputData);
+
+        // Then
+        assertThat(actual).isEqualTo(inputData);
     }
 }
