7db45673726d24dbc479f7b783ddba818f084a7b
Yevhen Tsehelnyk <yts@adorsys.com.ua>
2020-04-16 19:13:28 +0300

    #1254 Return correct error code in case payment was not found in PisCommonPaymentController Unused import was removed.

--- consent-management/consent-xs2a-web/src/test/java/de/adorsys/psd2/consent/web/xs2a/controller/PisCommonPaymentControllerTest.java
+++ consent-management/consent-xs2a-web/src/test/java/de/adorsys/psd2/consent/web/xs2a/controller/PisCommonPaymentControllerTest.java
@@ -22,7 +22,6 @@ import de.adorsys.psd2.consent.api.CmsResponse;
 import de.adorsys.psd2.consent.api.authorisation.*;
 import de.adorsys.psd2.consent.api.pis.CreatePisCommonPaymentResponse;
 import de.adorsys.psd2.consent.api.pis.PisCommonPaymentDataStatusResponse;
-import de.adorsys.psd2.consent.api.pis.proto.PisCommonPaymentRequest;
 import de.adorsys.psd2.consent.api.pis.proto.PisCommonPaymentResponse;
 import de.adorsys.psd2.consent.api.pis.proto.PisPaymentInfo;
 import de.adorsys.psd2.consent.api.service.AuthorisationServiceEncrypted;
