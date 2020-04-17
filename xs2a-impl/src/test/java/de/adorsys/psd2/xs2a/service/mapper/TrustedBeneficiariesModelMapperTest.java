b54f1310637fb5d809c57f03e6b0fe8e9da51dc9
Andrii Murashkin <amu@adorsys.com.ua>
2020-04-17 10:34:34 +0300

    simplified test

--- xs2a-impl/src/test/java/de/adorsys/psd2/xs2a/service/mapper/TrustedBeneficiariesModelMapperTest.java
+++ xs2a-impl/src/test/java/de/adorsys/psd2/xs2a/service/mapper/TrustedBeneficiariesModelMapperTest.java
@@ -16,34 +16,26 @@
 
 package de.adorsys.psd2.xs2a.service.mapper;
 
-import de.adorsys.psd2.model.Address;
 import de.adorsys.psd2.model.TrustedBeneficiaries;
 import de.adorsys.psd2.model.TrustedBeneficiariesList;
-import de.adorsys.psd2.xs2a.core.domain.address.Xs2aAddress;
 import de.adorsys.psd2.xs2a.domain.account.Xs2aTrustedBeneficiaries;
 import de.adorsys.psd2.xs2a.domain.account.Xs2aTrustedBeneficiariesList;
-import de.adorsys.psd2.xs2a.web.mapper.Xs2aAddressMapper;
+import de.adorsys.psd2.xs2a.web.mapper.Xs2aAddressMapperImpl;
 import de.adorsys.xs2a.reader.JsonReader;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = {TrustedBeneficiariesModelMapperImpl.class, TrustedBeneficiariesModelMapperTest.TestConfiguration.class})
+@ContextConfiguration(classes = {TrustedBeneficiariesModelMapperImpl.class, Xs2aAddressMapperImpl.class})
 class TrustedBeneficiariesModelMapperTest {
 
     @Autowired
     private TrustedBeneficiariesModelMapper trustedBeneficiariesModelMapper;
-    @Autowired
-    private Xs2aAddressMapper xs2aAddressMapper;
 
     private JsonReader jsonReader = new JsonReader();
 
@@ -52,9 +44,6 @@ class TrustedBeneficiariesModelMapperTest {
         // Given
         TrustedBeneficiaries expected = jsonReader.getObjectFromFile("json/service/mapper/trusted-beneficiaries-model-mapper/trusted-beneficiaries.json", TrustedBeneficiaries.class);
         Xs2aTrustedBeneficiaries input = jsonReader.getObjectFromFile("json/service/mapper/trusted-beneficiaries-model-mapper/xs2a-trusted-beneficiaries.json", Xs2aTrustedBeneficiaries.class);
-        Xs2aAddress xs2aAddress = jsonReader.getObjectFromFile("json/service/mapper/trusted-beneficiaries-model-mapper/xs2a-address.json", Xs2aAddress.class);
-        Address address = jsonReader.getObjectFromFile("json/service/mapper/trusted-beneficiaries-model-mapper/address.json", Address.class);
-        when(xs2aAddressMapper.mapToAddress(xs2aAddress)).thenReturn(address);
 
         // When
         TrustedBeneficiaries actual = trustedBeneficiariesModelMapper.mapToTrustedBeneficiaries(input);
@@ -67,25 +56,12 @@ class TrustedBeneficiariesModelMapperTest {
     void mapToTrustedBeneficiariesList() {
         // Given
         TrustedBeneficiariesList expected = jsonReader.getObjectFromFile("json/service/mapper/trusted-beneficiaries-model-mapper/trusted-beneficiaries-list.json", TrustedBeneficiariesList.class);
-
         Xs2aTrustedBeneficiariesList input = jsonReader.getObjectFromFile("json/service/mapper/trusted-beneficiaries-model-mapper/xs2a-trusted-beneficiaries-list.json", Xs2aTrustedBeneficiariesList.class);
-
-        Xs2aAddress xs2aAddress = jsonReader.getObjectFromFile("json/service/mapper/trusted-beneficiaries-model-mapper/xs2a-address.json", Xs2aAddress.class);
-        Address address = jsonReader.getObjectFromFile("json/service/mapper/trusted-beneficiaries-model-mapper/address.json", Address.class);
-        when(xs2aAddressMapper.mapToAddress(xs2aAddress)).thenReturn(address);
 
         // When
         TrustedBeneficiariesList actual = trustedBeneficiariesModelMapper.mapToTrustedBeneficiariesList(input);
 
         // Then
         assertThat(actual).isEqualTo(expected);
-    }
-
-    @Configuration
-    static class TestConfiguration {
-        @Bean
-        public Xs2aAddressMapper mockXs2aAddressMapper() {
-            return mock(Xs2aAddressMapper.class);
-        }
     }
 }
